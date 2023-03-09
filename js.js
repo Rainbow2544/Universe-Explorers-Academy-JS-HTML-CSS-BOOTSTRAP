20f331e965c6ebbecd62a046ded1b6ddfe9a0fcc
Rainbow <kenesou@gmail.com>
2023-03-10 02:25:10 +1300

    first commit

---
+++ js.js
@@ -0,0 +1,69 @@
+const showHome = () => {
+    document.getElementById("home").style.display = "block";
+    document.getElementById("study").style.display = "block";
+    document.getElementById("video").style.display = "block";
+    document.getElementById("blog").style.display = "block";
+    document.getElementById("gallery").style.display = "block";
+    document.getElementById("contact").style.display = "none";
+    document.getElementById("registration").style.display = "none";
+
+}
+const showStudy = () => {
+    document.getElementById("home").style.display = "block";
+    document.getElementById("study").style.display = "block";
+    document.getElementById("video").style.display = "block";
+    document.getElementById("blog").style.display = "block";
+    document.getElementById("gallery").style.display = "block";
+    document.getElementById("contact").style.display = "none";
+    document.getElementById("registration").style.display = "none";
+
+}
+const showVideo = () => {
+    document.getElementById("home").style.display = "block";
+    document.getElementById("study").style.display = "block";
+    document.getElementById("video").style.display = "block";
+    document.getElementById("blog").style.display = "block";
+    document.getElementById("gallery").style.display = "block";
+    document.getElementById("contact").style.display = "none";
+    document.getElementById("registration").style.display = "none";
+
+}
+const showBlog= () => {
+    document.getElementById("home").style.display = "block";
+    document.getElementById("study").style.display = "block";
+    document.getElementById("video").style.display = "block";
+    document.getElementById("blog").style.display = "block";
+    document.getElementById("gallery").style.display = "block";
+    document.getElementById("contact").style.display = "none";
+    document.getElementById("registration").style.display = "none";
+
+}
+const showGalley= () => {
+    document.getElementById("home").style.display = "block";
+    document.getElementById("study").style.display = "block";
+    document.getElementById("video").style.display = "block";
+    document.getElementById("blog").style.display = "block";
+    document.getElementById("gallery").style.display = "block";
+    document.getElementById("contact").style.display = "none";
+    document.getElementById("registration").style.display = "none";
+
+}
+const showRegistration = () => {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("study").style.display = "none";
+    document.getElementById("video").style.display = "none";
+    document.getElementById("blog").style.display = "none";
+    document.getElementById("gallery").style.display = "none";
+    document.getElementById("contact").style.display = "none";
+    document.getElementById("registration").style.display = "block";
+}
+
+const showContact = () => {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("study").style.display = "none";
+    document.getElementById("video").style.display = "none";
+    document.getElementById("blog").style.display = "none";
+    document.getElementById("gallery").style.display = "none";
+    document.getElementById("contact").style.display = "block";
+    document.getElementById("registration").style.display = "none";
+}
